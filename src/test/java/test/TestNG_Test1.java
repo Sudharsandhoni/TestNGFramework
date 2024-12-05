ac4ca085a2c34bedccb4840f9a456a3b27d308be
sudharsandhoni <sudharsandhonisalem@gmail.com>
2024-12-05 22:38:25 +0530

    Initial commit

---
+++ src/test/java/test/TestNG_Test1.java
@@ -0,0 +1,33 @@
+package test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestNG_Test1 {
+	
+	@BeforeClass
+	public void BeforeClass() {
+		System.out.println("Class Name: TestNG_Test1 - Hey i am Before Class");
+	}
+	
+	
+	@Test(groups = "smoke")
+	public void Test1() {
+		System.out.println("Class Name: TestNG_Test1 - Hey i am Test1 Tagged 'smoke'");
+	}
+	
+	@Test(groups = "smoke")
+	public void Test2() {
+		System.out.println("Class Name: TestNG_Test1 - Hey i am Test2 Tagged 'smoke'");
+	}
+	
+	@Test(groups = "smoke")
+	public void Test3() {
+		System.out.println("Class Name: TestNG_Test1 - Hey i am Test3 Tagged 'smoke'");
+	}
+	
+	@Test(groups = "regression")
+	public void Test4() {
+		System.out.println("Class Name: TestNG_Test1 - Hey i am Test4 Tagged 'regression'");
+	}
+
+}
